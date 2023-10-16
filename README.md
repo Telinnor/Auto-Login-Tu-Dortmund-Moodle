fdb4b311f3a7f7cf4df930445b44410790db9d00
Telinnor <31061339+Telinnor@users.noreply.github.com>
2023-10-16 09:44:17 +0200

    Version 0.1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Auto-Login-Tu-Dortmund-Moodle
+This Script is designt for the tampermonke extension. Just install this Script and save the password in the Browser to auto fill the credentials. 
---
+++ Script.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Auto Login TU Dortmund
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Script to auto login into moodle of the TU Dortmund
+// @author       Lars Wittemeier (lars.wittemeier@tu-dortmund.de)
+// @match        https://*.tu-dortmund.de/*
+// @icon         https://www.tu-dortmund.de/storages/administration/_processed_/4/7/csm_favicon-600x600_ccc9d33448.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log(window.location.href);
+    if(window.location.href=="https://moodle.tu-dortmund.de/login/index.php"){
+      document.getElementsByClassName("btn-primary")[0].click();
+    }
+
+    if(window.location.href=="https://sso.itmc.tu-dortmund.de/openam/XUI/?goto=https%3A%2F%2Fmoodle.tu-dortmund.de%2Flogin#login/"){
+        var loginInterval = setInterval(function() {
+                //console.log(document.getElementById("idToken1").value);
+            if(document.getElementById('idToken1') !==null){
+                console.log(document.getElementById("idToken1").value);
+                if(document.getElementById("idToken1").value != ""){
+                    document.getElementsByClassName("btn-primary")[0].click();
+                }
+                clearInterval(loginInterval);
+            }
+        }, 100);
+
+    }
+    //if(window.location.hostname)
+
+
+    // Your code here...
+})();
